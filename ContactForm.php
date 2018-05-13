ea336cf31b072922c9ec9e122df99c1e22eceaf7
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-05-13 07:20:28 +0000

    contact-form -- déploiement v2

--- ContactForm.php
+++ ContactForm.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstiFy
  * @namespace \tiFy\Plugins\ContactForm
- * @version 0.0.0
+ * @version 2.0.0
  */
 
 namespace tiFy\Plugins\ContactForm;
