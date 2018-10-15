aad9e32ed95c1b98fe04f5d3461fafd7ef68a516
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-10-15 13:40:52 +0000

    contact-form -- branch 2.0 -- bug config

--- ContactForm.php
+++ ContactForm.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/contact-form
  * @namespace \tiFy\Plugins\ContactForm
- * @version 2.0.0
+ * @version 2.0.1
  */
 
 namespace tiFy\Plugins\ContactForm;
